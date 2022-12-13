cbaaac31c406d5311ce68f490b3f2610e7c8ef48
italo-dartDigital <italo.cavalcante@dartdigital.com.br>
2022-12-13 10:20:23 -0300

    add components to storybooks

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'prettier/prettier': [
+        'error',
+        {
+            'endOfLine': 'auto',
+        }
+    ],
+    extends: ['@rocketseat/eslint-config/react'],
+}
